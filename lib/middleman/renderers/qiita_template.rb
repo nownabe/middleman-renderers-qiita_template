b298a9265116b7f11957b8eb51aab8ff869e1198
nownabe <nownabe@gmail.com>
2016-11-27 17:26:27 +0900

    :+1: v0.2.0

--- lib/middleman/renderers/qiita_template.rb
+++ lib/middleman/renderers/qiita_template.rb
@@ -6,7 +6,7 @@ require "tilt/qiita_template"
 module Middleman
   module Renderers
     class QiitaTemplate < ::Tilt::QiitaTemplate
-      VERSION = "0.1.1"
+      VERSION = "0.2.0"
     end
 
     ::Tilt.register QiitaTemplate, "markdown", "mkd", "md"
